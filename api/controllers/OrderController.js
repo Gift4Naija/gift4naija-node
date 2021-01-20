2588ccd1b12944efbfdc87cb1a2b381ed4cfbdc6
micaiah-effiong <micaiah.effiong@gmail.com>
2021-01-20 21:03:46 +0100

    setup fetch query

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -6,7 +6,34 @@
  */
 
 module.exports = {
-  
+  getAll: async (req, res) => {
+    const allOrders = await Order.find();
+    const ordersJsonData = await allOrders.map((user) => user.toJSON());
 
+    return res.json({
+      success: true,
+      data: ordersJsonData,
+    });
+  },
+
+  getOne: async (req, res) => {
+    const queryID = req.params.id;
+    const order = await Order.findOne().where({ id: queryID });
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        msg: "NotFound",
+      });
+    }
+
+    const orderJsonData = order.toJSON();
+    return res.json({
+      success: true,
+      data: orderJsonData,
+    });
+  },
+
+  update: async (req, res) => {},
+  remove: async (req, res) => {},
 };
-
