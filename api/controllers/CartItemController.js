74a5f85f72d3300a83f026ca1673b016a6c38090
micaiah-effiong <micaiah.effiong@gmail.com>
2021-01-20 21:18:57 +0100

    setting basic fetch actions

--- api/controllers/CartItemController.js
+++ api/controllers/CartItemController.js
@@ -6,7 +6,34 @@
  */
 
 module.exports = {
-  
+  getAll: async (req, res) => {
+    const allCartItems = await CartItem.find();
+    const cartItemsJsonData = await allCartItems.map((user) => user.toJSON());
 
+    return res.json({
+      success: true,
+      data: cartItemsJsonData,
+    });
+  },
+
+  getOne: async (req, res) => {
+    const queryID = req.params.id;
+    const cartItem = await CartItem.findOne().where({ id: queryID });
+
+    if (!cartItem) {
+      return res.status(404).json({
+        success: false,
+        msg: "NotFound",
+      });
+    }
+
+    const cartItemJsonData = cartItem.toJSON();
+    return res.json({
+      success: true,
+      data: orderJsonData,
+    });
+  },
+
+  update: async (req, res) => {},
+  remove: async (req, res) => {},
 };
-
