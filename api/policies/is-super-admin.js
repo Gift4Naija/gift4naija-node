7bfac714cbd04373620ee7cce965a90b7bfe1967
micaiah-effiong <micaiah.effiong@gmail.com>
2021-01-20 21:14:55 +0100

    code formatting and setting JSON response

--- api/policies/is-super-admin.js
+++ api/policies/is-super-admin.js
@@ -9,20 +9,18 @@
  *   https://sailsjs.com/docs/concepts/policies/access-control-and-permissions
  */
 module.exports = async function (req, res, proceed) {
-
   // First, check whether the request comes from a logged-in user.
   // > For more about where `req.me` comes from, check out this app's
   // > custom hook (`api/hooks/custom/index.js`).
   if (!req.me) {
     return res.unauthorized();
-  }//•
+  } //•
 
   // Then check that this user is a "super admin".
   if (!req.me.isSuperAdmin) {
     return res.forbidden();
-  }//•
+  } //•
 
   // IWMIH, we've got ourselves a "super admin".
   return proceed();
-
 };
--- api/responses/expired.js
+++ api/responses/expired.js
@@ -21,17 +21,17 @@
  * ```
  */
 module.exports = function expired() {
-
   var req = this.req;
   var res = this.res;
 
-  sails.log.verbose('Ran custom response: res.expired()');
+  sails.log.verbose("Ran custom response: res.expired()");
 
-  if (req.wantsJSON) {
+  /*  if (req.wantsJSON) {
     return res.status(498).send('Token Expired/Invalid');
   }
   else {
     return res.status(498).view('498');
-  }
+  }*/
 
+  return res.status(498).json({ success: false, msg: "Token Expired/Invalid" });
 };
