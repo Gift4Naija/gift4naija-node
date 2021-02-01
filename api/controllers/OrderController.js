0fabd4c9c0188de3f3944a6f230aabe6e46bcb45
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-01 23:10:50 +0100

    created endpoint controllers for admin and user. creating now sums price of items and empties user cart

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -6,16 +6,17 @@
  */
 
 module.exports = {
+  // for admin
   getAll: async (req, res) => {
     const allOrders = await Order.find();
-    const ordersJsonData = await allOrders.map((user) => user.toJSON());
 
     return res.json({
       success: true,
-      data: ordersJsonData,
+      data: allOrders,
     });
   },
 
+  // for admin
   getOne: async (req, res) => {
     const queryID = req.params.id;
     const order = await Order.findOne().where({ id: queryID });
@@ -23,7 +24,53 @@ module.exports = {
     if (!order) {
       return res.status(404).json({
         success: false,
-        msg: "NotFound",
+        errorType: "NotFound",
+        msg: "Not Found",
+      });
+    }
+
+    const orderJsonData = order.toJSON();
+    return res.json({
+      success: true,
+      data: orderJsonData,
+    });
+  },
+
+  // for users
+  getUserAll: async (req, res) => {
+    const queryID = req.params.id;
+    const order = await Order.findOne().where({
+      sender: req.me.id,
+    });
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        errorType: "NotFound",
+        msg: "Not Found",
+      });
+    }
+
+    const orderJsonData = order.toJSON();
+    return res.json({
+      success: true,
+      data: orderJsonData,
+    });
+  },
+
+  // for users
+  getUserOne: async (req, res) => {
+    const queryID = req.params.id;
+    const order = await Order.findOne().where({
+      id: queryID,
+      sender: req.me.id,
+    });
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        errorType: "NotFound",
+        msg: "Not Found",
       });
     }
 
@@ -35,35 +82,57 @@ module.exports = {
   },
 
   /*
-   * {Number} userID
-   * {Array} product - an array of product IDs
+   *
    */
   create: async (req, res) => {
     const { body } = req;
-    // find products from array,
-    const orderProducts = await Product.find({
-      id: req.body.products,
-    }).catch((err) => req.negotiate(err));
 
+    // get products from user cart,
+    const orderCartItem = await CartItem.find({
+      owner: req.me.id,
+    })
+      .populate("item")
+      .catch((err) => res.negotiate(err));
+
+    const orderProducts = orderCartItem.map(({ item }) => item);
+
+    const amount = orderProducts.reduce((a, b) => {
+      return a + b.price;
+    }, 0);
+    // generate order id
+    body.orderId = await sails.helpers.genOrderId();
     body.items = orderProducts;
+    body.sender = req.me.id;
+    body.amount = amount;
+
+    // validate receivers information
 
     const newOrder = await Order.create(body)
       .fetch()
-      .catch((err) => req.negotiate(err));
+      .catch((err) => res.negotiate(err));
 
     if (!newOrder) {
-      return res.status(402).json({ success: false, msg: "Bad Request" });
+      return res.badRequest();
     }
+
+    // empty user cart
+    const itemsToRemove = orderProducts.map(({ id }) => id);
+    await CartItem.destroy({ id: { in: itemsToRemove } }).catch((err) =>
+      res.negotiate(err)
+    );
 
     return res.status(201).json({
       success: true,
       data: newOrder,
-      msg: "Successfully created a product",
+      msg: "Successfully created an order",
     });
   },
 
   update: async (req, res) => {
-    const newOrder = await Order.updateOne({ id: req.params.id }).set(req.body);
+    const newOrder = await Order.updateOne({
+      id: req.params.id,
+      sender: req.me.id,
+    }).set(req.body);
 
     if (!newOrder) {
       return res.status(400).json({ success: false, msg: "Bad Request" });
@@ -77,7 +146,10 @@ module.exports = {
   },
 
   remove: async (req, res) => {
-    const removedOrder = await Order.destroyOne({ id: req.params.id });
+    const removedOrder = await Order.destroyOne({
+      id: req.params.id,
+      sender: req.me.id,
+    });
 
     if (!removedOrder) {
       return res.status(400).json({ success: false, msg: "Bad Request" });
@@ -86,7 +158,7 @@ module.exports = {
     return res.json({
       success: true,
       data: removedOrder,
-      msg: "Successfully removed a product",
+      msg: "Successfully removed an order",
     });
   },
 };
