930313aa55a0892f796f2b8448457d08e7a29dee
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-06 20:52:13 +0100

    added <quantity, amount> attribute

--- api/models/CartItem.js
+++ api/models/CartItem.js
@@ -2,14 +2,22 @@
  * CartItem.js
  *
  * @description :: A model definition represents a database table/collection.
- * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ * Only quantity can be updated by user
+ * amount is calculated in controller
  */
 
 module.exports = {
   attributes: {
-    // cart total amount
-    totalAmount: {
+    quantity: {
       type: "number",
+      defaultsTo: 1,
+    },
+
+    // cartItem total amount
+    // quantity * item.price
+    amount: {
+      type: "number",
+      required: true,
     },
 
     item: {
