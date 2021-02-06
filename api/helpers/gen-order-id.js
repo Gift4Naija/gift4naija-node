21407ea2cc6d993565f43c41198f3a007dab50b4
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-06 20:50:59 +0100

    code refactor

--- api/helpers/gen-order-id.js
+++ api/helpers/gen-order-id.js
@@ -1,12 +1,14 @@
-module.exports = { fn: genOrderId };
+const { nanoid } = require("nanoid/async");
 
 async function genOrderId() {
-  const { nanoid } = sails.config.globals.nanoid;
   const id = await nanoid();
-
   const order = await Order.find({ orderId: id });
 
+  // check for existing orders
   if (order.length) return genOrderId();
 
+  // return new id
   return id;
 }
+
+module.exports = { fn: genOrderId };
