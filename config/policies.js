e41e2e3ff56ec10b7d392fd38763a21140958222
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-08 21:22:10 +0100

    added user-controller to admin protected routes

--- config/policies.js
+++ config/policies.js
@@ -11,6 +11,7 @@
 module.exports.policies = {
   "*": "is-logged-in",
   OrderController: { getAll: "is-admin", getOne: "is-admin" },
+  UserController: { getAll: "is-admin", getOne: "is-admin" },
   ProductController: {
     create: "is-admin",
     update: "is-admin",
