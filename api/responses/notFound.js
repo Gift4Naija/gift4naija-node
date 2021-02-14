f0bf17f24bea232a7826e9ee2258fdc46088864e
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-14 10:52:14 +0100

    added some responses

---
+++ api/responses/notFound.js
@@ -0,0 +1,42 @@
+/**
+ * Module dependencies
+ */
+
+// n/a
+
+/**
+ * 404 (Not Found) Handler
+ *
+ * Usage:
+ * return res.notFound();
+ * return res.notFound(err);
+ * return res.notFound(err, 'some/specific/notfound/view');
+ *
+ * e.g.:
+ * ```
+ * return res.notFound();
+ * ```
+ *
+ * NOTE:
+ * If a request doesn't match any explicit routes (i.e. `config/routes.js`)
+ * or route blueprints (i.e. "shadow routes", Sails will call `res.notFound()`
+ * automatically.
+ */
+
+module.exports = function notFound() {
+  // Get access to `req` and `res`
+  var req = this.req;
+  var res = this.res;
+
+  // Get access to `sails`
+  var sails = req._sails;
+
+  // Set status code
+  res.status(404);
+
+  // If the request wants JSON, send back the appropriate status code.
+  return res.json({
+    success: false,
+    errorType: "Not Found",
+  });
+};
---
+++ api/responses/serverError.js
@@ -0,0 +1,38 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require("@sailshq/lodash");
+var flaverr = require("flaverr");
+
+/**
+ * 500 (Server Error) Response
+ *
+ * Usage:
+ * return res.serverError();
+ * return res.serverError(err);
+ * return res.serverError(err, 'some/specific/error/view');
+ *
+ * NOTE:
+ * If something throws in a policy or controller, or an internal
+ * error is encountered, Sails will call `res.serverError()`
+ * automatically.
+ */
+
+module.exports = function serverError(err, msg) {
+  // Get access to `req` and `res`
+  var req = this.req;
+  var res = this.res;
+
+  // Get access to `sails`
+  var sails = req._sails;
+
+  // Set status code
+  res.status(500);
+
+  return res.json({
+    errorType: "Server Error",
+    error: err,
+    msg: msg,
+  });
+};
