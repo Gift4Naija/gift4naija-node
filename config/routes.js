b33c37a6d271747f8fad2670f7a4b521816caa11
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-16 21:14:01 +0100

    added endpoint for send-email-confirmation

--- config/routes.js
+++ config/routes.js
@@ -28,6 +28,9 @@ module.exports.routes = {
   "POST  /api/v1/entrance/send-password-recovery-email": {
     action: "entrance/send-password-recovery-email",
   },
+  "POST  /api/v1/entrance/send-email-confirmation": {
+    action: "entrance/send-email-confirmation",
+  },
   "POST  /api/v1/entrance/update-password-and-login": {
     action: "entrance/update-password-and-login",
   },
