2dafc9dc9a7a32f241bc63248f008a89b7980356
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-16 18:48:53 +0100

    code format.

--- api/controllers/deliver-contact-form-message.js
+++ api/controllers/deliver-contact-form-message.js
@@ -1,58 +1,48 @@
 module.exports = {
+  friendlyName: "Deliver contact form message",
 
-
-  friendlyName: 'Deliver contact form message',
-
-
-  description: 'Deliver a contact form message to the appropriate internal channel(s).',
-
+  description:
+    "Deliver a contact form message to the appropriate internal channel(s).",
 
   inputs: {
-
     emailAddress: {
       required: true,
-      type: 'string',
-      description: 'A return email address where we can respond.',
-      example: 'hermione@hogwarts.edu'
+      type: "string",
+      description: "A return email address where we can respond.",
+      example: "hermione@hogwarts.edu",
     },
 
     topic: {
       required: true,
-      type: 'string',
-      description: 'The topic from the contact form.',
-      example: 'I want to buy stuff.'
+      type: "string",
+      description: "The topic from the contact form.",
+      example: "I want to buy stuff.",
     },
 
     fullName: {
       required: true,
-      type: 'string',
-      description: 'The full name of the human sending this message.',
-      example: 'Hermione Granger'
+      type: "string",
+      description: "The full name of the human sending this message.",
+      example: "Hermione Granger",
     },
 
     message: {
       required: true,
-      type: 'string',
-      description: 'The custom message, in plain text.'
-    }
-
+      type: "string",
+      description: "The custom message, in plain text.",
+    },
   },
-
 
   exits: {
-
     success: {
-      description: 'The message was sent successfully.'
-    }
-
+      description: "The message was sent successfully.",
+    },
   },
 
-
-  fn: async function({emailAddress, topic, fullName, message}) {
-
+  fn: async function ({ emailAddress, topic, fullName, message }) {
     if (!sails.config.custom.internalEmailAddress) {
       throw new Error(
-`Cannot deliver incoming message from contact form because there is no internal
+        `Cannot deliver incoming message from contact form because there is no internal
 email address (\`sails.config.custom.internalEmailAddress\`) configured for this
 app.  To enable contact form emails, you'll need to add this missing setting to
 your custom config -- usually in \`config/custom.js\`, \`config/staging.js\`,
@@ -60,7 +50,7 @@ your custom config -- usually in \`config/custom.js\`, \`config/staging.js\`,
       );
     }
 
-    await sails.helpers.sendTemplateEmail.with({
+    /*await sails.helpers.sendTemplateEmail.with({
       to: sails.config.custom.internalEmailAddress,
       subject: 'New contact form message',
       template: 'internal/email-contact-form',
@@ -71,9 +61,19 @@ your custom config -- usually in \`config/custom.js\`, \`config/staging.js\`,
         topic,
         message,
       }
+    });*/
+
+    await EmailService({
+      to: sails.config.custom.internalEmailAddress,
+      subject: "New contact form message",
+      text: `client contact - contactName: ${fullName}, contactEmail: ${emailAddress}, topic: ${topic}.
+
+        message: ${message}`,
+    }).catch((err) => console.log(err));
+
+    return this.res.json({
+      success: true,
+      msg: "The message was sent successfully.",
     });
-
-  }
-
-
+  },
 };
