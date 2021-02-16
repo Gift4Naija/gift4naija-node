e77398c82009e1fdb899f8baf0a990290c8342ed
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-16 18:56:10 +0100

    code refactor - user query is removed. token is attach to <userId>

--- api/hooks/custom/decodeJwtAuthorization.js
+++ api/hooks/custom/decodeJwtAuthorization.js
@@ -1,18 +1,16 @@
 module.exports = async function decodeJwtAuthorization(req) {
-  if (!(req.header("authorization") || req.header("Authorization"))) {
+  const authorization =
+    req.header("authorization") || req.header("Authorization");
+
+  if (!authorization) {
     return;
   }
 
-  const token =
-    req.header("authorization").split("Bearer ")[1] ||
-    req.header("Authorization").split("Bearer ")[1];
+  const token = authorization.split("Bearer ")[1];
 
   if (!token) {
     return;
   }
-
-  /*  const { verify } = sails.helpers.jwt();
-  let payload = await verify(token).catch((err) => new Error(err));*/
 
   const payload = await sails.helpers
     .jwt()
@@ -23,10 +21,6 @@ module.exports = async function decodeJwtAuthorization(req) {
     return;
   }
 
-  const user = await User.findOne({
-    id: payload.token,
-  });
-
-  req.user = user;
+  req.userId = payload.token;
   return;
 };
