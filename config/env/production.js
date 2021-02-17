ee6161acefb390aa038a8f64188c36a207ad7f50
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-17 13:07:40 +0100

    set migration option from env variable

--- config/env/production.js
+++ config/env/production.js
@@ -81,7 +81,7 @@ module.exports = {
      * https://sailsjs.com/docs/concepts/models-and-orm/model-settings#?migrate *
      *                                                                          *
      ***************************************************************************/
-    migrate: "alter",
+    migrate: process.env.DB_MIGRATE /*"alter"*/,
 
     /***************************************************************************
      *                                                                          *
