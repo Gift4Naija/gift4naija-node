e8c4a1b0815c2cebc587d73b1a146b536fff1745
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-19 14:22:50 +0100

    used sails default negotiator

--- api/responses/negotiate.js
+++ api/responses/negotiate.js
@@ -1,77 +1,33 @@
-/**
- * negotiate.js
- *
- * A custom response.
- *
- * Example usage:
- * ```
- *     return res.negotiate();
- *     // -or-
- *     return res.negotiate(optionalData);
- * ```
- *
- * Or with actions2:
- * ```
- *     exits: {
- *       somethingHappened: {
- *         responseType: 'negotiate'
- *       }
- *     }
- * ```
- *
- * ```
- *     throw 'somethingHappened';
- *     // -or-
- *     throw { somethingHappened: optionalData }
- * ```
- */
-
-module.exports = function negotiate(optionalData) {
-  // Get access to `req` and `res`
+module.exports = function negotiate(err) {
+  // Get access to request (`req`), response (`res`), and Sails app (`sails`).
   var req = this.req;
   var res = this.res;
+  var sails = req._sails;
 
-  const resData = {
-    success: false,
-  };
+  sails.log.debug(
+    "`res.negotiate` is deprecated.  Use a custom response instead (see http://sailsjs.com/docs/concepts/custom-responses).\n"
+  );
 
-  // Define the status code to send in the response.
-  var statusCodeToSet = 400;
+  var statusCode = 500;
+  var body = err;
 
-  res.status(statusCodeToSet);
+  try {
+    statusCode = err.status || 500;
 
-  // If no data was provided, use res.sendStatus().
-  if (optionalData === undefined) {
-    sails.log.info("Ran custom response: res.negotiate()");
-    return res.json(resData);
+    // Set the status
+    // (should be taken care of by res.* methods, but this sets a default just in case)
+    res.status(statusCode);
+  } catch (unusedErr) {}
+
+  // Respond using the appropriate custom response
+  if (statusCode === 403) {
+    return res.forbidden(body);
   }
-  // Else if the provided data is an Error instance, if it has
-  // a toJSON() function, then always run it and use it as the
-  // response body to send.  Otherwise, send down its `.stack`,
-  // except in production use res.sendStatus().
-  else if (_.isError(optionalData)) {
-    sails.log.info(
-      "Custom response `res.negotiate()` called with an Error:",
-      optionalData
-    );
-    resData.error = optionalData;
-    return res.json(resData);
-
-    // If the error doesn't have a custom .toJSON(), use its `stack` instead--
-    // otherwise res.json() would turn it into an empty dictionary.
-    // (If this is production, don't send a response body at all.)
-    /*    if (!_.isFunction(optionalData.toJSON)) {
-      if (process.env.NODE_ENV === 'production') {
-        return res.sendStatus(statusCodeToSet);
-      }
-      else {
-        return res.status(statusCodeToSet).send(optionalData.stack);
-      }
-    }*/
+  if (statusCode === 404) {
+    return res.notFound(body);
   }
-  // Set status code and send response data.
-  else {
-    resData.msg = optionalData;
-    return res.json(resData);
+  if (statusCode >= 400 && statusCode < 500) {
+    return res.badRequest(body);
   }
+  return res.serverError(body);
 };
